9c8441f78af8bb2efa92e0159a95fd85b90bad80
Nicolás Bravo <146019744+nicobravorp@users.noreply.github.com>
2025-05-15 16:58:43 +0000

    Update Binarize.scm

--- Binarize.scm
+++ Binarize.scm
@@ -1,33 +1,67 @@
-(define (binarize imagen drawable umbral)
+(define (string-suffix? suffix str)
+  (let ((slen (string-length suffix))
+        (len (string-length str)))
+    (and (<= slen len)
+         (string=? (substring str (- len slen) len) suffix))))
+
+(define (get-filename-without-extension path)
+  (let* ((parts (strbreakup path "/"))
+         (filename (list-ref parts (- (length parts) 1)))
+         (name-parts (strbreakup filename "."))
+         (base (if (> (length name-parts) 1)
+                   (list-ref name-parts 0)
+                   filename)))
+    base))
+
+(define (script-fu-binarize-from-file input-path output-dir umbral)
+  (gimp-message "Inicio de script")
+
   (let* (
-          (copia (car (gimp-layer-copy drawable TRUE)))
+         (image (car (gimp-file-load RUN-NONINTERACTIVE input-path input-path)))
+         (layers (gimp-image-get-layers image))
+         (layer (if (and (vector? layers) (> (vector-length layers) 0))
+                    (aref layers 0)
+                    (begin
+                      (gimp-message "Vector vacío: usando gimp-image-flatten")
+                      (car (gimp-image-flatten image)))))
+         (copy (car (gimp-layer-copy layer TRUE)))
+         (basename (get-filename-without-extension input-path))
+         (output-dir-clean (if (string-suffix? "/" output-dir)
+                               (substring output-dir 0 (- (string-length output-dir) 1))
+                               output-dir))
+         (output-name (string-append output-dir-clean "/" basename "_binarized.png"))
         )
 
-    ; Insertar la capa duplicada
-    (gimp-image-insert-layer imagen copia 0 -1)
+    (gimp-message (string-append "DEBUG output-name = [" output-name "]"))
 
-    ; Convertir a escala de grises
-    (gimp-drawable-desaturate copia DESATURATE-LIGHTNESS)
+    (gimp-image-insert-layer image copy 0 -1)
+    (gimp-message "Capa preparada")
 
-    ; Aplicar umbral fijo
-    (gimp-drawable-threshold copia HISTOGRAM-VALUE (/ umbral 255.0) 1.0)
+    (gimp-drawable-desaturate copy DESATURATE-LIGHTNESS)
+    (gimp-message "Desaturación aplicada")
 
-    ; Refrescar la vista
-    (gimp-displays-flush)
+    (gimp-drawable-threshold copy HISTOGRAM-VALUE (/ umbral 255.0) 1.0)
+    (gimp-message "Umbral aplicado")
+
+    (gimp-file-save RUN-NONINTERACTIVE image output-name #f)
+    (gimp-message (string-append "Guardado en: " output-name))
+
+    (gimp-image-delete image)
+    (gimp-message "Imagen eliminada de la memoria")
   )
 )
 
 (script-fu-register
-  "binarize"
-  "Binarize"
-  "Convierte una imagen a blanco y negro usando umbral fijo."
-  "Nicolas Bravo"
-  "Nicolas Bravo"
-  "2025"
-  "RGB*, GRAY*"
-  SF-IMAGE      "Imagen"         0
-  SF-DRAWABLE   "Capa"           0
-  SF-ADJUSTMENT "Umbral"         '(128 0 255 1 10 0 0)
+ "script-fu-binarize-from-file"
+ "Binarize From File"
+ "Carga una imagen, aplica binarización y guarda el resultado como PNG."
+ "Nicolás Bravo"
+ "Nicolás Bravo"
+ "2025"
+ ""
+ SF-FILENAME "Archivo de imagen" ""
+ SF-DIRNAME  "Directorio de salida" ""
+ SF-ADJUSTMENT "Umbral" '(128 0 255 1 10 0 0)
 )
 
-(script-fu-menu-register "binarize" "<Image>/Filters/Custom")
+(script-fu-menu-register "script-fu-binarize-from-file" "<Image>/Filters/Custom")
